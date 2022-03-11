0723520ba2c04f1d891cbb89c278068c053cd0fa
rahk1990 <rahkem.ellis@gmail.com>
2022-03-11 15:01:48 -0500

    add function for PUT and DELETE request

--- songs_app/views.py
+++ songs_app/views.py
@@ -25,5 +25,23 @@ def songs_list(request):
         serializer.save()
         return Response(serializer.data, status.HTTP_201_CREATED)
 
+@api_view(['GET', 'PUT', 'DELETE'])
+def song_detail(request, pk):
+   
+    song = get_object_or_404(Song, pk=pk)
+    
+    if request.method == 'GET':
+            
+            serializer = SongSerializer(song)
+            return Response(serializer.data)
+            
+    elif request.method == 'PUT':
+            serializer = SongSerializer(song, data=request.data)
+            serializer.is_valid(raise_exception=True)
+            serializer.save()
+            return Response(serializer.data)
 
+    elif request.method == 'DELETE':
+            song.delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
      
